2e10b9ce5f99878a75dc392e0a3efaa24b43fdcd
samueltcsantos <thiagsamuel@gmail.com>
2014-09-20 08:41:04 -0300

    chamado o autoload no index .php dentro do framework-1

--- src/framework-1/v0.1.0/index.php
+++ src/framework-1/v0.1.0/index.php
@@ -4,3 +4,10 @@ header('Content-Type: text/html; charset=utf-8');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
+
+
+require 'library' . DS . 'STCDS' . DS . 'core' . DS . 'AutoLoad.php';
+
+$autoLoad = new AutoLoad();
+
+spl_autoload_register(array($AutoLoad, 'core'));
