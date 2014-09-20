6dd6104e6213fb2e0d69df3d5c6298c545c4d140
samueltcsantos <thiagsamuel@gmail.com>
2014-09-20 08:57:58 -0300

    adicionado o código basico da view.

--- src/framework-1/v0.1.0/Library/STCDS/Core/View.php
+++ src/framework-1/v0.1.0/Library/STCDS/Core/View.php
@@ -2,5 +2,15 @@
 namespace STCDS\Core;
 
 class View {
-
+	
+	private $model;
+	
+	public function __construct($model){
+		$this->model = $model;
+	}
+	
+	public function __destruct()
+	{
+		echo '<p><a href="?action=clicked">' . $this->model->string. "</a></p>";
+	}
 }
