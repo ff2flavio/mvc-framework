370479746a42d7b179a27f71673284b57bcde840
samueltcsantos <thiagsamuel@gmail.com>
2014-09-20 08:50:52 -0300

    controlador carregando agora o model e a view.

--- src/framework-1/v0.1.0/Library/STCDS/Core/Controller.php
+++ src/framework-1/v0.1.0/Library/STCDS/Core/Controller.php
@@ -3,5 +3,12 @@
 namespace STCDS\Core;
 
 class Controller {
-
+	
+	protected $model;
+	protected $view;
+	
+	public function __construct(){
+		$this->model = new Model();
+		$this->view = new View($this->model);
+	}
 }
