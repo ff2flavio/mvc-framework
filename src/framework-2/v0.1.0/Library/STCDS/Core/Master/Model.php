8d8520a2da8861a8351fc42e29d393189485a7b7
samueltcsantos <thiagsamuel@gmail.com>
2014-09-20 10:17:22 -0300

    redefinida a estrutura

---
+++ src/framework-2/v0.1.0/Library/STCDS/Core/Master/Model.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Model mestre, deve ser extendido pela aplicação
+ *
+ * PHP version 5
+ *
+ * @category STCDS
+ * @package  Master
+ * @author   Samuel T. C. Santos <thiagsamuel@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/samueltcsantos/mvc-framework
+ *
+ */
+ 
+namespace STCDS\Core;
+
+use STCDS\Utilities\Inflector;
+
+use App\Core\Config;
+
+/**
+ * Master_Model
+ *
+ * @category STCDS
+ * @package  Master
+ * @author   Samuel T. C. Santos <thiagsamuel@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/samueltcsantos/mvc-framework
+ *
+ */
+class Master_Model
+{
+}
---
+++ src/framework-2/v0.1.0/Library/STCDS/Core/Router.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Roteamento define qual Controller, Model e View deve ser executado
+ *
+ * PHP version 5
+ *
+ * @category STCS
+ * @package  Core
+ * @author   Samuel T. C. Santos <thiagsamuel@gmail>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/samueltcsantos/mvc-framework/
+ *
+ */
+
+namespace STCDS\Core;
+
+ /**
+ * Router
+ *
+ * @category STCDS
+ * @package  Core
+ * @author   Samuel T. C. Santos <thiagsamuel@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/samueltcsantos/mvc-framework
+ *
+ */
+class Router
+{
+	
+}
